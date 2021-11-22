6c99d8b0f9484e995948e4980a5ec7afeff4cfd2
Modestas Rukšnaitis <94378898+ModisAtHMRC@users.noreply.github.com>
2021-11-22 13:31:08 +0000

    RK-305 made POC API: accepts JSON, turns to XML, responds with XSD validation result

--- app/uk/gov/hmrc/taxfraudreporting/controllers/MicroserviceHelloWorldController.scala
+++ app/uk/gov/hmrc/taxfraudreporting/controllers/MicroserviceHelloWorldController.scala
@@ -16,16 +16,53 @@
 
 package uk.gov.hmrc.taxfraudreporting.controllers
 
-import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import org.micchon.playjsonxml.Xml
+import org.xml.sax.InputSource
+import play.api.Environment
+import play.api.libs.json.JsValue
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import java.io.StringReader
 import javax.inject.{Inject, Singleton}
+import javax.xml.XMLConstants
+import javax.xml.transform.sax.SAXSource
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
 import scala.concurrent.Future
+import scala.language.postfixOps
+import scala.util.Try
 
-@Singleton()
-class MicroserviceHelloWorldController @Inject() (cc: ControllerComponents) extends BackendController(cc) {
+@Singleton
+class MicroserviceHelloWorldController @Inject() (
+  cc: ControllerComponents,
+  env: Environment,
+  ApiAction: ApiActionBuilder
+) extends BackendController(cc) {
 
-  def hello(): Action[AnyContent] = Action.async { implicit request =>
+  def hello: Action[AnyContent] = Action.async {
     Future.successful(Ok("Hello world"))
+  }
+
+  def postReport: Action[JsValue] =
+    ApiAction(parse.json) { implicit request =>
+      val reportXml = <fraudReport>{Xml.toXml(request.body)}</fraudReport>
+      val xmlString = s"""<?xml version="1.0"?>$reportXml"""
+      val source    = new SAXSource(new InputSource(new StringReader(xmlString)))
+
+      val schemaFactory = SchemaFactory newInstance XMLConstants.W3C_XML_SCHEMA_NS_URI
+      val schemaFile    = env.classLoader getResourceAsStream "fraudReport.xsd"
+      val schema        = schemaFactory newSchema new StreamSource(schemaFile)
+      val validator     = schema newValidator
+
+      Try(validator validate source).fold(
+        exception => BadRequest(s"Could not validate fraud report against schema: ${exception.getMessage}"),
+        _ => Ok(reportXml)
+      )
+    }
+
+  def handleOptions: Action[AnyContent] = ApiAction {
+    NoContent.withHeaders("Access-Control-Allow-Headers" -> "Content-type", "Accept" -> "application/json")
   }
 
 }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.16.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.56.0"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.16.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.56.0",
+    "org.micchon"       %% "play-json-xml"             % "0.4.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.16.0"             % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.56.0"            % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.0.0"             % "test,it",
-    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.3.0"           % Test
-
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.16.0"  % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.56.0"  % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8"  % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.0.0"   % "test,it",
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.3.0" % Test
   )
+
 }
