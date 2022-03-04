57defd45a0d395e5ee795c4e70d8d363fa604f2b
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2022-03-04 11:04:22 +0000

    Update platform dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % "5.16.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.56.0",
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % "5.20.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.60.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.12.4",
     "com.github.java-json-tools"    % "json-schema-validator"     % "2.2.14",
     "uk.gov.hmrc.objectstore"      %% "object-store-client-play-28" % "0.39.0",
@@ -13,8 +13,8 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.16.0"  % Test,
-    "uk.gov.hmrc"            %% "government-gateway-test" % "4.6.0-play-28" % "test,it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.20.0"  % Test,
+    "uk.gov.hmrc"            %% "government-gateway-test" % "4.8.0" % "test,it",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.56.0"  % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8"  % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.0.0"   % "test,it",
